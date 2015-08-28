652470eb488c7f0cb37dfdf138940b94210de367
BrandonArroyo <barroyo50@gmail.com>
2015-08-28 13:34:29 -0500

    + graph to the file

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-angular.module("contextioChallenge",[]).
+angular.module("contextioChallenge",['nvd3']).
 controller("indexController", function($scope, $http){
 
   var messagesUrl = "/api/messages/";
@@ -173,4 +173,65 @@ controller("indexController", function($scope, $http){
         .text(function(d) { return d;})
     });
   }
+
+  // NVD3 stuff
+
+  $scope.options = {chart: {
+                type: 'scatterChart',
+                height: 450,
+                color: d3.scale.category10().range(),
+                scatter: {
+                    onlyCircles: false
+                },
+                showDistX: true,
+                showDistY: true,
+                tooltipContent: function(key) {
+                    return '<h3>' + key + '</h3>';
+                },
+                transitionDuration: 350,
+                xAxis: {
+                    axisLabel: 'X Axis',
+                    tickFormat: function(d){
+                        return d3.format('.02f')(d);
+                    }
+                },
+                yAxis: {
+                    axisLabel: 'Y Axis',
+                    tickFormat: function(d){
+                        return d3.format('.02f')(d);
+                    },
+                    axisLabelDistance: 30
+                }
+            }
+          };
+
+       /* Chart data */
+       $scope.data = generateData(4,40);
+
+        /* Random Data Generator (took from nvd3.org) */
+        function generateData(groups, points) {
+            var data = [],
+                shapes = ['circle', 'cross', 'triangle-up', 'triangle-down', 'diamond', 'square'],
+                random = d3.random.normal();
+
+            for (var i = 0; i < groups; i++) {
+                data.push({
+                    key: 'Group ' + i,
+                    values: []
+                });
+
+                for (var j = 0; j < points; j++) {
+                    data[i].values.push({
+                        x: random()
+                        , y: random()
+                        , size: Math.random()
+                        , shape: shapes[j % 6]
+                    });
+                }
+            }
+            return data;
+        }
+  
+
+
 });
